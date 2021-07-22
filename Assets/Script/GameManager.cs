3b34401b788921eeea32ce718a1906910f356fae
유원석 <dbdnjstjr613@gmal.com>
2021-07-22 15:13:18 +0900

    중간저장

--- Assets/Script/GameManager.cs
+++ Assets/Script/GameManager.cs
@@ -1,18 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    GameObject[] GraphicCards;
+
+    [SerializeField]
+    Transform EnemyPosition;
+
+    Sequence sequence = null;
+
+    int CurruntCard = 0;
+
+    int ClickCount = 0;
+
+    public void AttackEnemy()
     {
-        
+        ClickCount++;
+
+        if (sequence != null)
+            sequence.Kill();
+        sequence = DOTween.Sequence();
+
+        CurruntCard = Random.Range(0, GraphicCards.Length);
+        sequence.Append(GraphicCards[CurruntCard].transform.DOMove(EnemyPosition.position, 0.1f));
+        sequence.AppendInterval(0.1f);
+        sequence.Append(GraphicCards[CurruntCard].transform.DOMove(GraphicCards[CurruntCard].transform.parent.position, 0.1f));
+
+        sequence.AppendCallback(() =>
+        {
+            
+        });
     }
 
-    // Update is called once per frame
-    void Update()
+    public void Update()
     {
-        
+        if (ClickCount > 0)
+        {
+
+        }
     }
 }
