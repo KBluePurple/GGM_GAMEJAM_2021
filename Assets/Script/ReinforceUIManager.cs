69325fc7ac5d64bc924924815b426bb9becfdfee
zanke27 <zanke27@gmail.com>
2021-07-22 15:55:28 +0900

    그래픽 매니저, 업그레이드 시스템 업데이트

--- Assets/Script/ReinforceUIManager.cs
+++ Assets/Script/ReinforceUIManager.cs
@@ -2,17 +2,45 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ReinforceUIManager : MonoBehaviour
+public class Upgrade : MonoBehaviour
 {
-    // Start is called before the first frame update
+    GraphicCardManager graphicCardManager = null;
+    MoneyManager moneyManager = null;
     void Start()
     {
-        
+        graphicCardManager = FindObjectOfType<GraphicCardManager>();
+        moneyManager = FindObjectOfType<MoneyManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelUpCheck1()
     {
-        
+        if (graphicCardManager.graphicCard1Level * 100 >= moneyManager.money) return;
+        moneyManager.money -= graphicCardManager.graphicCard1Level * 100;
+        graphicCardManager.LevelUp1();
+    }
+
+    public void LevelUpCheck2()
+    {
+        if (graphicCardManager.graphicCard2Level * 500 >= moneyManager.money) return;
+        moneyManager.money -= graphicCardManager.graphicCard2Level * 500;
+        graphicCardManager.LevelUp2();
+    }
+    public void LevelUpCheck3()
+    {
+        if (graphicCardManager.graphicCard3Level * 1000 >= moneyManager.money) return;
+        moneyManager.money -= graphicCardManager.graphicCard3Level * 1000;
+        graphicCardManager.LevelUp3();
+    }
+    public void LevelUpCheck4()
+    {
+        if (graphicCardManager.graphicCard4Level * 2500 >= moneyManager.money) return;
+        moneyManager.money -= graphicCardManager.graphicCard4Level * 2500;
+        graphicCardManager.LevelUp4();
+    }
+    public void LevelUpCheck5()
+    {
+        if (graphicCardManager.graphicCard5Level * 5000 >= moneyManager.money) return;
+        moneyManager.money -= graphicCardManager.graphicCard5Level * 5000;
+        graphicCardManager.LevelUp5();
     }
 }
