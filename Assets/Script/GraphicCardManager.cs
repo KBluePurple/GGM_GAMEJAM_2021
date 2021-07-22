4584638b82573483b214045e2cf8381cde73c988
zanke27 <zanke27@gmail.com>
2021-07-22 15:15:55 +0900

    글카매니저

--- Assets/Script/GraphicCardManager.cs
+++ Assets/Script/GraphicCardManager.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class GraphicCardManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public double graphicCard1Level = 1;
+    public double graphicCard1Damage = 1;
+
+    public double graphicCard2Level = 1;
+    public double graphicCard2Damage = 2.5;
+
+    public double graphicCard3Level = 1;
+    public double graphicCard3Damage = 5;
+
+    public double graphicCard4Level = 1;
+    public double graphicCard4Damage = 10;
+
+    public double graphicCard5Level = 1;
+    public double graphicCard5Damage = 25;
+
+    public void LevelUp1()
     {
-        
+        graphicCard1Level += 1;
+        graphicCard1Damage += 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void LevelUp2()
     {
-        
+        graphicCard2Level += 1;
+        graphicCard2Damage += 2.5;
+    }
+    public void LevelUp3()
+    {
+        graphicCard3Level += 1;
+        graphicCard3Damage += 5;
+    }
+    public void LevelUp4()
+    {
+        graphicCard4Level += 1;
+        graphicCard4Damage += 10;
+    }
+    public void LevelUp5()
+    {
+        graphicCard5Level += 1;
+        graphicCard5Damage += 25;
     }
 }
