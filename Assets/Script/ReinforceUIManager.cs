0eb0d91d5ce8d4f46038b1456e6615a647a2e1af
유원석 <dbdnjstjr613@gmal.com>
2021-07-23 22:40:59 +0900

    no message

--- Assets/Script/ReinforceUIManager.cs
+++ Assets/Script/ReinforceUIManager.cs
@@ -4,52 +4,51 @@ using UnityEngine;
 
 public class ReinforceUIManager : MonoBehaviour
 {
-    [SerializeField]
-    GraphicCard[] GraphicCards;
+    public GraphicCard[] GraphicCards;
 
     [SerializeField]
     double[] reinforcecost = new double[] { 1, 50, 100, 100, 250, 300, 400, 500, 600, 800, 1000};
 
-    GameManager gameManager = null;
+    MoneyManager moneyManager = null;
     void Start()
     {
-        gameManager = FindObjectOfType<GameManager>();
+        moneyManager = FindObjectOfType<MoneyManager>();
     }
 
 
     public void LevelUpCheck1()
     {
-        if (GraphicCards[0].level * 100 >= gameManager.money) return;
-        gameManager.money -= GraphicCards[0].level * 100;
+        if (GraphicCards[0].level * 100 >= moneyManager.money) return;
+        moneyManager.money -= GraphicCards[0].level * 100;
         GraphicCards[0].levelUp();
         reinforcecost[0] *= 100;
     }
 
     public void LevelUpCheck2()
     {
-        if (GraphicCards[1].level * 500 >= gameManager.money) return;
-        gameManager.money -= GraphicCards[1].level * 500;
+        if (GraphicCards[1].level * 500 >= moneyManager.money) return;
+        moneyManager.money -= GraphicCards[1].level * 500;
         GraphicCards[1].levelUp();
         reinforcecost[1] *= 500;
     }
     public void LevelUpCheck3()
     {
-        if (GraphicCards[2].level * 1000 >= gameManager.money) return;
-        gameManager.money -= GraphicCards[2].level * 1000;
+        if (GraphicCards[2].level * 1000 >= moneyManager.money) return;
+        moneyManager.money -= GraphicCards[2].level * 1000;
         GraphicCards[2].levelUp();
         reinforcecost[2] *= 1000;
     }
     public void LevelUpCheck4()
     {
-        if (GraphicCards[3].level * 2500 >= gameManager.money) return;
-        gameManager.money -= GraphicCards[3].level * 2500;
+        if (GraphicCards[3].level * 2500 >= moneyManager.money) return;
+        moneyManager.money -= GraphicCards[3].level * 2500;
         GraphicCards[3].levelUp();
         reinforcecost[3] *= 2500;
     }
     public void LevelUpCheck5()
     {
-        if (GraphicCards[4].level * 5000 >= gameManager.money) return;
-        gameManager.money -= GraphicCards[4].level * 5000;
+        if (GraphicCards[4].level * 5000 >= moneyManager.money) return;
+        moneyManager.money -= GraphicCards[4].level * 5000;
         GraphicCards[4].levelUp();
         reinforcecost[4] *= 5000;
     }
--- Assets/Script/UIManager.cs
+++ Assets/Script/UIManager.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -14,12 +14,25 @@ public class UIManager : MonoBehaviour
     [SerializeField]
     Text[] UpgradeValues;
 
-    GameManager gameManager;
+    MoneyManager moneyManager;
 
-    
+    ReinforceUIManager reinforceUIManager;
+
+    void Start()
+    {
+        moneyManager = FindObjectOfType<MoneyManager>();
+        reinforceUIManager = FindObjectOfType<ReinforceUIManager>();
+    }
 
     public void UpdateUI()
     {
-        
+        MoneyText.text = string.Format("{0}₩", moneyManager.money);
+        MoneyText2.text = string.Format("{0}₩", moneyManager.debt);
+
+        UpgradeValues[0].text = string.Format("{0}₩", reinforceUIManager.GraphicCards[0].level * 100);
+        UpgradeValues[1].text = string.Format("{0}₩", reinforceUIManager.GraphicCards[1].level * 500);
+        UpgradeValues[2].text = string.Format("{0}₩", reinforceUIManager.GraphicCards[2].level * 1000);
+        UpgradeValues[3].text = string.Format("{0}₩", reinforceUIManager.GraphicCards[3].level * 2500);
+        UpgradeValues[4].text = string.Format("{0}₩", reinforceUIManager.GraphicCards[4].level * 5000);
     }
 }
