a76b7e139c11e151a60a4a58d90b00003df9d3f6
snowgaru <lootmusic999@gmail.com>
2021-07-23 23:31:35 +0900

    no message

--- Assets/Script/PopManager.cs
+++ Assets/Script/PopManager.cs
@@ -6,13 +6,19 @@ using DG.Tweening;
 
 public class PopManager : MonoBehaviour
 {
+ 
+   
+
     public Transform reinforceError;
 
     public Button reinforceErrorBackturnbtn;
+
     void Start()
     {
         reinforceErrorBackturnbtn.onClick.AddListener(ReinforceErrorBackturn);
         reinforceError.localScale = Vector3.zero;
+
+        
     }
 
     public void ReinforceError()
@@ -31,4 +37,38 @@ public class PopManager : MonoBehaviour
         Sequence seq = DOTween.Sequence();
         seq.Append(reinforceError.DOScale(new Vector3(0f, 0f, 0f), 0.2f));
     }
+
+    
+
+
+    /*
+     public Transform makingError;
+
+    public Button makingBackturnbtn;
+
+
+
+
+    makingBackturnbtn.onClick.AddListener(makingBackturn);
+        makingError.localScale = Vector3.zero;
+
+
+
+    public void makingError()
+    {
+        makingError.gameObject.SetActive(true);
+        Sequence seq = DOTween.Sequence();
+        seq.Append(makingError.DOScale(new Vector3(1.2f, 1.2f, 1.2f), 0.2f));
+        seq.Append(makingError.DOScale(new Vector3(1f, 1f, 1f), 0.1f));
+
+        //DOTween.To(겟터, 셋터, 최종값, 기간);
+        ///DOTween.To(() => cvsGroup.alpha, v => cvsGroup.alpha = v, 1, 2.5f);
+    }
+
+    public void makingBackturn()
+    {
+        Sequence seq = DOTween.Sequence();
+        seq.Append(makingError.DOScale(new Vector3(0f, 0f, 0f), 0.2f));
+    }
+    */
 }
