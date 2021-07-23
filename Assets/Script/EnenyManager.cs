a20982a63d237ea7e6b0b4992aacc2d3bc8e8975
zanke27 <zanke27@gmail.com>
2021-07-23 10:13:40 +0900

    적 체력 배열로 설정

--- Assets/Script/EnenyManager.cs
+++ Assets/Script/EnenyManager.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class EnenyManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public double enemyLevel = 1;
+    double[] enemyHp = new double[] { 1, 50, 100, 250, 300, 400, 500, 600, 800, 1000};
+
+    public void EnemmyOneHpCheck()
     {
-        
+        enemyHp[0] = enemyLevel * 10;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void EnemmyTwoHpCheck()
     {
-        
+        enemyHp[1] = enemyLevel * 50;
+    }
+    public void EnemmyThreeHpCheck()
+    {
+        enemyHp[2] = enemyLevel * 100;
+    }
+    public void EnemmyFourHpCheck()
+    {
+        enemyHp[3] = enemyLevel * 250;
+    }
+    public void EnemmyFiveHpCheck()
+    {
+        enemyHp[4] = enemyLevel * 300;
+    }
+    public void EnemmySixHpCheck()
+    {
+        enemyHp[5] = enemyLevel * 400;
+    }
+    public void EnemmySevenHpCheck()
+    {
+        enemyHp[6] = enemyLevel * 500;
+    }
+    public void EnemmyEightHpCheck()
+    {
+        enemyHp[7] = enemyLevel * 600;
+    }
+    public void EnemmyNineHpCheck()
+    {
+        enemyHp[8] = enemyLevel * 800;
+    }
+    public void EnemmyTenHpCheck()
+    {
+        enemyHp[9] = enemyLevel * 1000;
     }
 }
