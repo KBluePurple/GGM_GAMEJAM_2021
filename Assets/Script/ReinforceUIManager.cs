3e7031299b830e3b8d92cdd7a5fe6fb8bb1b060a
유원석 <dbdnjstjr613@gmal.com>
2021-07-23 22:15:25 +0900

    수정

--- Assets/Script/ReinforceUIManager.cs
+++ Assets/Script/ReinforceUIManager.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Upgrade : MonoBehaviour
+public class ReinforceUIManager : MonoBehaviour
 {
     [SerializeField]
     GraphicCard[] GraphicCards;
---
+++ Assets/Script/UIManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : MonoBehaviour
+{
+    [SerializeField]
+    Text MoneyText;
+
+    [SerializeField]
+    Text MoneyText2;
+
+    [SerializeField]
+    Text[] UpgradeValues;
+
+    GameManager gameManager;
+
+    
+
+    public void UpdateUI()
+    {
+        
+    }
+}
