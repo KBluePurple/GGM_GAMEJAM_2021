f0067a44a98c692818124c90b589c385fd14beb4
zanke27 <zanke27@gmail.com>
2021-07-23 21:36:23 +0900

    돈 저장 엔딩 장면 에셋에 넣기

--- Assets/Script/MoneyManager.cs
+++ Assets/Script/MoneyManager.cs
@@ -4,5 +4,31 @@ using UnityEngine;
 
 public class MoneyManager : MonoBehaviour
 {
-    public double money = 0;
+    public int debt = 250000;
+    public int money = 0;
+
+    private void Start()
+    {
+        LoadMoneyData();
+    }
+
+    public void DebtCheck()
+    {
+        if (money <= 250000) return;
+        money -= debt;
+
+    }
+
+    public void SaveData()
+    {
+        PlayerPrefs.SetInt("Money", money);
+        PlayerPrefs.Save();
+    }
+
+    public void LoadMoneyData()
+    {
+        if (PlayerPrefs.HasKey("Money"))
+            return;
+        money = PlayerPrefs.GetInt("Money");
+    }
 }
