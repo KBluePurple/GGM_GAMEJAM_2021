2d161f14a90762545f0050a1f4b70aa39dbff8ea
유원석 <dbdnjstjr613@gmal.com>
2021-07-23 12:45:51 +0900

    no message

--- Assets/Script/AttackManager.cs
+++ Assets/Script/AttackManager.cs
@@ -9,8 +9,6 @@ public class AttackManager : MonoBehaviour
     GameObject[] GraphicCards;
     [SerializeField]
     Transform EnemyPosition;
-    [SerializeField]
-    Camera uiCamera = null;
     int clickCount = 0;
 
 
@@ -25,26 +23,29 @@ public class AttackManager : MonoBehaviour
         {
             Sequence DOSequence = DOTween.Sequence();
             List<GameObject> idleCard = new List<GameObject>();
+
             for (int i = 0; i < GraphicCards.Length; i++)
             {
                 if (!GraphicCards[i].GetComponent<GraphicCard>().IsAttacking)
                     idleCard.Add(GraphicCards[i]);
             }
 
-            int selectedCard = Random.Range(0, idleCard.Count);
-            idleCard[selectedCard].GetComponent<GraphicCard>().IsAttacking = true;
-
-            DOSequence.Append(idleCard[selectedCard].transform.DOMove(EnemyPosition.position, 0.1f));
-            DOSequence.AppendInterval(0.1f);
-            DOSequence.Append(idleCard[selectedCard].transform.DOMove(new Vector3(0, 0, 0) + idleCard[selectedCard].transform.parent.position, 0.1f));
-            Camera.main.GetComponent<ShakeCamera>().VibrateForTime(0.1f);
-
-            DOSequence.InsertCallback(0.3f, () =>
+            if (idleCard.Count > 0)
             {
-                idleCard[selectedCard].GetComponent<GraphicCard>().IsAttacking = false;
-                DOSequence.Kill();
-            });
+                int selectedCard = Random.Range(0, idleCard.Count);
+                idleCard[selectedCard].GetComponent<GraphicCard>().IsAttacking = true;
 
+                DOSequence.Append(idleCard[selectedCard].transform.DOMove(EnemyPosition.position, 0.1f));
+                DOSequence.AppendInterval(0.1f);
+                DOSequence.Append(idleCard[selectedCard].transform.DOMove(new Vector3(0, 0, 0) + idleCard[selectedCard].transform.parent.position, 0.1f));
+                Camera.main.GetComponent<ShakeCamera>().VibrateForTime(0.1f);
+
+                DOSequence.InsertCallback(0.3f, () =>
+                {
+                    idleCard[selectedCard].GetComponent<GraphicCard>().IsAttacking = false;
+                    DOSequence.Kill();
+                });
+            }
             clickCount--;
         }
     }
